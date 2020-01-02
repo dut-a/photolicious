592976260c01bdf7b56b329e4298e385dd8e1454
dut-a <d.dut@live.com>
2020-01-02 16:36:37 -0600

    Fixes 'photos' routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
+
+  homepage = "photos#index"
+
+  root homepage # Landing page
+
+  # Photos
+  get "/photos", to: homepage
+  post "/photos", to: "photos#create"
+  get "/photos/new", to: "photos#new", as: "new_photo"
+  get "/photos/:id/edit", to: "photos#edit", as: "edit_photo"
+  get "/photos/:id", to: "photos#show", as: "photo"
+  patch "/photos/:id", to: "photos#update"
+  get "/photos/:id/del", to: "photos#delete"
+  delete "/photos/:id", to: "photos#destroy"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
