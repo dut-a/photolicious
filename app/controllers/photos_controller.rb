fd1c2255723d466388d20bb145a3a1b95eb691b2
dut-a <d.dut@live.com>
2020-01-02 16:41:14 -0600

    Adds 'Photos' base files

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,67 @@
+class PhotosController < ApplicationController
+
+  def index
+    @photos = Photo.all
+  end
+
+  def new
+    @photo = Photo.new
+  end
+
+  def create
+    @photo = Photo.new(photo_params)
+    if @photo.valid?
+      @photo.save
+      redirect_to(photo_path(@photo))
+    else
+      render('new')
+    end
+  end
+
+  def show
+    @photo = find_photo
+  end
+
+  def edit
+    @photo = find_photo
+  end
+
+  def update
+    @photo = find_photo
+    if @photo.update_attributes(photo_params)
+      redirect_to(photo_path(@photo))
+    else
+      render(:edit)
+    end
+  end
+
+  def delete
+
+  end
+
+  def destroy
+    
+  end
+
+  private
+    def photo_params
+      params.require(:photo).permit(
+        :title,
+        :remote_url,
+        :description,
+        :copyright,
+        :taken_on,
+        :taken_in,
+        :season_id,
+        :photographer_id
+      )
+    end
+  
+    def find_photo
+      Photo.find_by(id: params[:id])
+    end
+
+end
+
+
+
