64ec07a041064c66c31c03eb696d3e0585f53fda
dut-a <d.dut@live.com>
2020-01-03 01:07:24 -0600

    Adds validations

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -4,9 +4,17 @@ class Photo < ApplicationRecord
 
   belongs_to :photographer
   belongs_to :season
-  has_many :photo_edits
+  has_many :photo_edits, :dependent => :destroy
   has_many :edits, through: :photo_edits
   has_many :editing_programs, through: :edits
+
+  # Validations
+  validates :title, presence: true, length: {minimum: 3}
+  validates :image, presence: true, file_size: { less_than: 10.megabytes }
+  validates :description, presence: true, length: {minimum: 5}
+  validates :season_id, presence: true
+  validates :photographer_id, presence: true
+
 
   # scopes
   scope :sorted, lambda { order("title ASC") }
--- app/models/photographer.rb
+++ app/models/photographer.rb
@@ -1,8 +1,11 @@
 class Photographer < ApplicationRecord    
-    has_many :photos
+    has_many :photos, :dependent => :destroy
     has_many :seasons, through: :photos
 
 
+    # Validations
+    validates :first_name, presence: true, length: {minimum: 2}
+    validates :last_name, presence: true, length: {minimum: 2}
 
     def name
         return "#{first_name} #{last_name}"
